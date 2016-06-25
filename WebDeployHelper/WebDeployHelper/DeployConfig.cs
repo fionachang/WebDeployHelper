e53245981377e9d9023917451d748f639fbd0984
fionachang <fiona.chang.w@gmail.com>
2016-06-25 14:17:36 +0800

    Set up project

---
+++ WebDeployHelper/WebDeployHelper/DeployConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WebDeployHelper
+{
+    class DeployConfig
+    {
+        public static readonly string DirConfig = Environment.CurrentDirectory + @"\WebDeployHelper.conf";
+
+        public string ConfigDirUpload;
+        public string ConfigReleaseType;
+        public string ConfigSftpAddress;
+        public string ConfigSftpUser;
+        public string ConfigDevPath;
+        public string ConfigReleasePath;
+
+        public void VerifyConfig()
+        {
+            if (ConfigReleaseType != "release" && ConfigReleaseType != "dev")
+            {
+                Util.DisplayWarning(TextCollection.Const.ErrorInvalidConfig + " Release type not correct.", new Exception());
+            }
+        }
+    }
+}
